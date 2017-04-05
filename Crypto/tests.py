c96615ce29bfef04f1f5216143b400b329baf807
icydoge <icydoge@gmail.com>
2017-04-05 13:52:18 +0100

    More test fixes.

--- Crypto/tests.py
+++ Crypto/tests.py
@@ -1,7 +1,7 @@
 import Crypto.encryption
 
 import nacl.secret
-from os import urandom
+from os import urandom, remove
 
 def test_encryption():
     """ Test the encryption module. """
@@ -22,13 +22,16 @@ def test_encryption():
         tests_passed += 1
 
     print("Testing encryption...")
-    tests_total += 3
-    test_message = urandom(2048)
-    encrypted_message = test_encryptor.encrypt(test_message)
-    decrypted_message = test_encryptor.decrypt(encrypted_message)
-    if decrypted_message == test_message:
-        tests_passed += 1
+    plain_text_sizes = [0, 2048, 2048576]
+    tests_total += len(plain_text_sizes)
+    for s in plain_text_sizes:
+        test_message = urandom(s)
+        encrypted_message = test_encryptor.encrypt(test_message)
+        decrypted_message = test_encryptor.decrypt(encrypted_message)
+        if decrypted_message == test_message:
+            tests_passed += 1
 
+    tests_total += 2
     invalid_message = "123"
     try:
         invalid_encryption = test_encryptor.encrypt(invalid_message)
@@ -45,9 +48,10 @@ def test_encryption():
     test_keys_strs = ["111", "11111111111111111111111111111111", "1111111111111111111111111111111111111111111111111111111111111111"]
     test_keys_bytes = [str.encode(i) for i in test_keys_strs]
     invalid_test_key = 123
-    if all(map(lambda x: len(x) == 32, (map(test_encryptor.derive_32_byte_key, test_keys_strs)))):
+    size = nacl.secret.SecretBox.KEY_SIZE
+    if all(map(lambda x: len(x) == size, (map(test_encryptor.derive_32_byte_key, test_keys_strs)))):
         tests_passed += 1
-    if all(map(lambda x: len(x) == 32, (map(test_encryptor.derive_32_byte_key, test_keys_bytes)))):
+    if all(map(lambda x: len(x) == size, (map(test_encryptor.derive_32_byte_key, test_keys_bytes)))):
         tests_passed += 1
 
     try:
@@ -59,5 +63,29 @@ def test_encryption():
 
     return tests_passed
 
+
+def test_file():
+
+    print("Testing file encrytions...")
+    test_file = open("temp.txt",'wb+')
+    test_text = str.encode("This is a test.\nThis is a test.")
+    test_key = urandom(32)
+    test_encryptor = Crypto.encryption.Encryption(test_key)
+    encrypted_text = test_encryptor.encrypt(test_text)
+    test_file.write(encrypted_text)
+    test_file.close()
+    test_file = open("temp.txt",'rb')
+    test_file_content = test_file.read()
+    test_decryptor = Crypto.encryption.Encryption(test_key)
+    test_read = test_decryptor.decrypt(test_file_content)
+    test_file.close()
+    remove("temp.txt")
+    if test_read == test_text:
+        print("File operations passed 1/1 test.")
+        return 1
+    else:
+        return 0
+
 if __name__ == '__main__':
     test_encryption()
+    test_file()
